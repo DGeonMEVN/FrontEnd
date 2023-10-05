87462b167d3cf8bd3104b8d2c894a7e872850a31
ovmkas <ovmkas@gmail.com>
2023-10-05 17:21:37 +0900

    express로 보내는 요청은 api로 묶어서 target을 지정해주고 보낸다 추후 주소가 바껴도 관리가 편하다

--- vite.config.js
+++ vite.config.js
@@ -13,8 +13,17 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      "/signup": "http://localhost:8080", // 프록시 설정\
-      "/auth/login": "http://localhost:8080",
+      "/api":
+        {
+          target: "http://localhost:8080",
+          changeOrigin: true,
+          pathRewrite: {
+            '^/api': ''
+          },
+          //   },
+          // "/signup": "http://localhost:8080", // 프록시 설정\
+          // "/auth/login": "http://localhost:8080",
+        },
     },
-  },
+  }
 });
